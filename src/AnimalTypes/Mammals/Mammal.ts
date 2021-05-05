cdd4168f465e6c0aa125ae0f37cdd24c915b0e18
Pawel Kozka <pawelkozka10@gmail.com>
2021-05-05 12:12:35 +0200

    remove console logs

--- src/AnimalTypes/Mammals/Mammal.ts
+++ src/AnimalTypes/Mammals/Mammal.ts
@@ -3,7 +3,6 @@ import { eBreathingType } from "../../enums/eBreathingType";
 
 export abstract class Mammal extends Animal {
     breathe():string {
-        console.log(`Breathe with ${eBreathingType.lungs}`, 'dupaaa')
         return `Breathe with ${eBreathingType.lungs}`
     }
 }
